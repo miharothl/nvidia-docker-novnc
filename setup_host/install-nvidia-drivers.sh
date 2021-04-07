508e70f35046b7eb4f35d747ee24d2671ad8678b
Ubuntu <ubuntu@ip-172-31-3-39.eu-west-1.compute.internal>
2021-04-07 09:05:57 +0000

    20.04 aws

--- setup_host/install-nvidia-drivers.sh
+++ setup_host/install-nvidia-drivers.sh
@@ -1,3 +1,11 @@
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.25/NVIDIA-Linux-x86_64-390.25.run
-chmod a+x NVIDIA-Linux-x86_64-390.25.run
-./NVIDIA-Linux-x86_64-390.25.run -s --install-libglvnd
+#wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.25/NVIDIA-Linux-x86_64-390.25.run
+#chmod a+x NVIDIA-Linux-x86_64-390.25.run
+#./NVIDIA-Linux-x86_64-390.25.run -s --install-libglvnd
+
+
+
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/460.67/NVIDIA-Linux-x86_64-460.67.run
+chmod a+x NVIDIA-Linux-x86_64-460.67.run
+./NVIDIA-Linux-x86_64-460.67.run -s --install-libglvnd
+
+
